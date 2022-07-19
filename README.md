8379c656fe0ae1cdb6cf81a3273b36ce62f15b87
Saad Siddiqui <k163751@nu.edu.pk>
2022-07-19 14:55:17 +0500

    Removed inline styles and added a font test

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 #To dos
-1. Change paddings for mobile sizes (currently px-7)
-2. Code Projects Section
-3. Test Responsiveness Project Section
-4. Code Footer
-5. Test Responsiveness Footer
-6. Test Responsiveness Full
+1. Change paddings for mobile sizes (currently px-7) -> DONE
+2. Code Projects Section -> DONE
+3. Test Responsiveness Project Section -> DONE
+4. Code Footer -> DONE
+5. Test Responsiveness Footer -> DONE
+6. Test Responsiveness Full -> DONE 
 7. Optimize, build css & create accessibility
 8. Deepsource Analyze the Project
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['myFirstFont'],
+      
+     
+    },
+    extend: {
+      flex: {
+        '2': '1 0 auto'
+      },
+      maxWidth: {
+        '3/5': '60%',
+        '37': '37vw',
+        '26ch' : "26ch",
+        '40ch' : "40ch"
+      },
+      width: {
+        'inherit': 'inherit',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
