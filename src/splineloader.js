aab913e78efcb36cd0149a02e0beefa9622a3080
Saad Siddiqui <k163751@nu.edu.pk>
2022-07-22 12:57:01 +0500

    project 1

--- src/splineloader.js
+++
@@ -1,8 +0,0 @@
-import { Application } from './resources/packages/@splinetool/runtime';
-
-// make sure you have a canvas in the body
-const canvas = document.getElementById('canvas3d');
-
-// start the application and load the scene
-const spline = new Application(canvas);
-spline.load('https://prod.spline.design/mI1kUWJsswhyDvKk/scene.splinecode');
